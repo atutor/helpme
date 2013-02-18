17e422be25e70c05e32b00135428177f2cea099d
Greg Gay <ggay@ocad.ca>
2013-02-18 10:54:13 -0500

    remove commented code

--- module.php
+++ module.php
@@ -27,11 +27,7 @@ if (admin_authenticate(AT_ADMIN_PRIV_HELPME, TRUE) || admin_authenticate(AT_ADMI
 
 global $_config, $_custom_head;
 $helpme_enabled = queryDB("SELECT dir_name from %smodules WHERE dir_name='helpme' && status='2'", array(TABLE_PREFIX), TRUE);
-//$custom_css = $_base_href.'mods/helpme/module.css';
-//$_custom_head .= '<link rel="stylesheet" href="'.$_base_href.'mods/helpme/module.css" type="text/css" />'."\n";
-//$_custom_head .= '<script type="text/javascipt" src="'.$_base_href.'mods/helpme/helpme_js.php"></script>'."\n";
 
-//if(!empty($helpme_enabled) && !preg_match("#/modules/index.php#", $_SERVER['PHP_SELF'])){
 if(!empty($helpme_enabled)){
 	if(!isset($_config['disable_helpme'])){
 		require("helpme_js.php");
